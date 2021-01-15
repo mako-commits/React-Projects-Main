c19101c1d1534c5609aab81d9ad3bfd2d5781f6e
pythagoras-dev <mubarakmustapha15@gmail.com>
2021-01-15 17:07:18 +0100

    added background patterns

--- Insure Landing Page/src/components/Intro.js
+++ Insure Landing Page/src/components/Intro.js
@@ -5,16 +5,19 @@ import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container'
 import introLeftMb from '../images/bg-pattern-intro-left-mobile.svg'
 import introRightMb from '../images/bg-pattern-intro-right-mobile.svg'
+import introLeftDesktop from '../images/bg-pattern-intro-left-desktop.svg'
+import introRightDesktop from '../images/bg-pattern-intro-right-desktop.svg'
 function Intro() {
     return (
         <>
             <section id='intro'>
-                <img src={introBgMobile} alt='img' className='introBgMobile' />
-                <img src={introBgDesktop} alt='img' className='introBgDesktop' />
+                <img src={introBgMobile} alt='img' className='introBgMobile ' />
+                <img src={introBgDesktop} alt='img' className='introBgDesktop  d-none d-lg-block' />
                 <Grid container>
                     <Grid item xs={12} lg={6}>
+                        <img src={introRightDesktop} alt='img' className='introRightDesktop d-lg-block d-none  ' />
                         <Container maxWidth='sm'>
-                            <div className='desktop-intro-section'>
+                            <div className='desktop-intro-section '>
                                 <div className="intro-text-area-inner">
                                     <h2 className="intro-main">Humanizing <br />your insurance.</h2>
                                     <p className="intro-text">Get your life insurance coverage easier and faster. We blend our expertise
@@ -26,16 +29,18 @@ function Intro() {
                             </div>
 
                         </Container>
+                        <img src={introLeftDesktop} alt='img' className='introLeftDesktop d-lg-block d-none' />
                         <div className='mobile-intro-section'>
                             <Container>
-                                <div className="mb-intro-text-area-inner">
-                                    {/* <img src={introLeftMb} alt='img' className='introLeftMb' /> */}
+                                <div className="mb-intro-text-area-inner ">
+
                                     <h2 className="intro-main">Humanizing <br />your insurance.</h2>
                                     <p className="intro-text">Get your life insurance coverage easier and faster. We blend our expertise
                                     and technology to help you find the plan that’s right for you. Ensure you
                                 and your loved ones are protected.</p>
 
                                     <button className="intro-btn">View Plans</button>
+
                                 </div>
                             </Container>
 
